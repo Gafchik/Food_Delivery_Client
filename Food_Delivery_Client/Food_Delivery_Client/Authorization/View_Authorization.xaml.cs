40534eb1fe8289864139f021dc31a368d41cd738
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-04-30 02:18:17 +0300

    qq

--- Food_Delivery_Client/Food_Delivery_Client/Authorization/View_Authorization.xaml.cs
+++ Food_Delivery_Client/Food_Delivery_Client/Authorization/View_Authorization.xaml.cs
@@ -14,8 +14,7 @@ namespace Food_Delivery_Client.Authorization
     public partial class View_Authorization : ContentPage
     {
         public View_Authorization()
-        {
-          
+        {         
             InitializeComponent();
             BindingContext = new ModelView_Authorization() { Navigation = this.Navigation }; // нваигатион создать во вьюмодел
         
